ceb6d7fecd7943b92fa351e53044b380272abfff
Jared Mulder <jared-mulder@uiowa.edu>
2025-02-25 14:33:52 -0600

    Set up some SPI code

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <wiringPi.h>
 #include <wiringPiSPI.h>
 
 //Constants:
 const int SENSITIVITY = 13; //Sensitivity (inverse gain) in nT/LSB.
+
+//Globals:
+uint8_t *buffer = (uint8_t*)malloc(4);
 
 typedef struct float3 {
     float x;
@@ -11,14 +16,19 @@ typedef struct float3 {
     float z;
 } float3_t;
 
-float3_t getAmbientField() {
-
+int getField(char axis) {
+    if (axis == 'x') buffer[0] = 0x80 | 0x24;
+    else if (axis == 'y') buffer[0] = 0x80 | 0x27;
+    else if (axis == 'z') buffer[0] = 0x80 | 0x2A;
+    wiringPiSPIDataRW(0, buffer, 4); //Should place the results in the 3 bytes that follow (HOPEFULLY).
+    return buffer[1] + (buffer[2] << 8) + (buffer[3] << 16); //???
 }
 
 int main(int argc, char** argv) {
     //setup:
     //The RM3100 wants CPOL=CPHA, so mode 0 or 3. 
-    wiringPiSPIxSetupMode(0,0,9600,3); //Device=0?, Number=0?, Speed=9600?, Mode=3. Do not alter unless you know what you're doing!
+    wiringPiSPISetupMode(0,9600,3); //Channel=0?, Speed=9600?, Mode=3. Do not alter unless you know what you're doing!
+
 
     //loop:
     return 0;
