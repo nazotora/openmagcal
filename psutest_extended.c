c4e018428c7b72f4fafed182bf56ebe91e08cf6a
Jared Mulder <jared-mulder@uiowa.edu>
2025-05-01 12:46:06 -0500

    Added safety rails to stop illegal values from being input

--- psutest_extended.c
+++ psutest_extended.c
@@ -57,7 +57,7 @@ void printRefB() {
     float bY = (float)refB[1] / 1000;
     float bZ = (float)refB[2] / 1000;
     float bT = sqrtf(bX*bX + bY*bY + bZ*bZ);
-    printf("B = %.3fx%+.3fy%+.3fz uT (|B| = %.3f)\n",bX, bY, bZ, bT);
+    printf("[DEBUG] B = %.3fx%+.3fy%+.3fz uT (|B| = %.3f)\n",bX, bY, bZ, bT);
 }
 
 void updateOrder(int signal) {
@@ -67,14 +67,25 @@ void updateOrder(int signal) {
     }
 
     double x = 0.0, y = 0.0, z = 0.0; //These are immediately initialized for safety reasons.
-    int t = 0, scan = 0;
+    int t = 0, invalid = FALSE;
     do {
+        invalid = FALSE;
         printf("Enter new command:\n\t");
         fgets(ioBuffer, 255, stdin); //Place the input line into a buffer for safety.
         ////Note: We may want to flush out stdin after in case someone enters a line that is more than 255 characters long!
-        scan = sscanf(ioBuffer, "%lf %lf %lf %d", &x, &y, &z, &t);
-        if (scan != 4) printf("Error: Malformed input!\n");
-    } while (scan != 4); //Make sure we actually have all the variables!
+
+        //Read input and check for invalid values:
+        if (sscanf(ioBuffer, "%lf %lf %lf %d", &x, &y, &z, &t) != 4) {
+            printf("Error: Malformed input!\n");
+            invalid = TRUE;
+        } else if (fabs(x) > 160 || fabs(y) > 160 || fabs(z) > 160) {
+            printf("Error: Magnetic field magnitude should not exceed 160 uT!\n");
+            invalid = TRUE;
+        } else if (t < 0 || t > 86400) { //Less than 0 seconds or More than 1 day.
+            printf("Error: Invalid time parameter!\n");
+            invalid = TRUE;
+        }
+    } while (invalid); //Make sure we actually have all the variables!
 
     latestOrder = fieldOrderNode_create(x, y, z, t); //Since latestOrder has been freed, it is safe to reuse the pointer.
 
@@ -86,7 +97,16 @@ void updateField() {
     double iX = (1000 * latestOrder->x - (double)refB[0]) * SKEW[0];
     double iY = (1000 * latestOrder->y - (double)refB[1]) * SKEW[1];
     double iZ = (1000 * latestOrder->z - (double)refB[2]) * SKEW[2];
-    printf("[DEBUG] Sending <%1.6f, %1.6f, %1.6f> to the PSU\n", iX, iY, iZ); //Debug print
+    #if DEBUG
+    printf("[DEBUG] Currents set to <%1.6f, %1.6f, %1.6f> A\n", iX, iY, iZ); //Debug print
+    #endif
+    //Check to make sure that field does not exceed safe parameters, and stop it if it does:
+    if (fabs(iX) > 0.6 || fabs(iY) > 0.6 || fabs(iZ) > 0.6) {
+        fprintf(stderr, "WARNING: Maximum current exceeded safe parameters (600 mA). All currents set to 0.\n");
+        iX = 0.0;
+        iY = 0.0;
+        iZ = 0.0;
+    }
     //Send the currents to the PSU:
     setAxisCurrent(fabs(iX), fabs(iY), fabs(iZ));
     //Send the current sign to the pins:
@@ -104,7 +124,7 @@ int main(int argc, char** argv) {
     sigemptyset(&termAction.sa_mask);
     sigaction(SIGINT, &termAction, NULL); //The termination handler is currently tied to SIGINT because that is usually how we are stopping it.
 
-    printf("Initializing \"Queue\"...\n");
+    printf("Initializing Queue...\n");
     ioBuffer = (char*)calloc(sizeof(char), 256);
     latestOrder = (fieldOrderNode_t*)calloc(sizeof(fieldOrderQueue_t*), 1);
 
@@ -136,6 +156,9 @@ int main(int argc, char** argv) {
     ////loop:
     while (1) {
         readRefB(); //Updates refB[].
+        #if DEBUG
+        printRefB();
+        #endif
 
         updateField(); //Updates what the PSU has set.
 
